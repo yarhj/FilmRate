852bc3d0f20a8c60e69441952155c236923a2a9b
yarhj <jarkusurov@gmail.com>
2025-05-03 18:51:23 +0300

    parse films

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, abort
 from forms.login_form import LoginForm
 from forms.register_form import RegisterForm
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 import uuid
+import requests
+from flask.cli import AppGroup
 from data.users import User
 from dotenv import load_dotenv
 from werkzeug.utils import secure_filename
@@ -16,14 +18,65 @@ app = Flask(__name__)
 db_session.global_init("db/database.db")
 
 load_dotenv()
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+TMDB_API_KEY = os.getenv("TMDB_API_KEY")
+
+cli = AppGroup('tmdb')
+app.cli.add_command(cli)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 login_manager.login_view = 'login'
+
+
+@cli.command('import_popular')
+def import_popular():
+    db_sess = db_session.create_session()
+    for page in range(1, 6):
+        url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={TMDB_API_KEY}&language=ru&page={page}"
+        response = requests.get(url)
+        data = response.json()
+
+        for movie in data['results']:
+            existing_film = db_sess.query(Films).filter(Films.title == movie['title']).first()
+            if existing_film:
+                print(f"Фильм '{movie['title']}' уже есть в БД, пропускаем...")
+                continue
+
+            details_url = f"https://api.themoviedb.org/3/movie/{movie['id']}?api_key={TMDB_API_KEY}&language=ru"
+            details_data = requests.get(details_url).json()
+            credits_url = f"https://api.themoviedb.org/3/movie/{movie['id']}/credits?api_key={TMDB_API_KEY}"
+            credits_data = requests.get(credits_url).json()
+            director = next(
+                (person['name'] for person in credits_data['crew'] if person['job'] == 'Director'),
+                "Неизвестно"
+            )
+            screenwriter = next(
+                (person['name'] for person in credits_data['crew'] if person['job'] == 'Writer'),
+                "Неизвестно"
+            )
+            film = Films(
+                title=movie['title'],
+                description=movie.get('overview', 'Нет описания'),
+                director=director,
+                screenwriter=screenwriter,
+                duration=details_data.get('runtime', 0),
+                average_rating=movie.get('vote_average', 0),
+                premiere=movie.get('release_date', '')[:4],
+                poster_path=f"https://image.tmdb.org/t/p/w500{movie['poster_path']}" if movie.get('poster_path') else None,
+                user_id=1
+            )
+            db_sess.add(film)
+            print(f"Добавлен фильм: {movie['title']}")
+
+        db_sess.commit()
+        print('Импорт завершен')
+
+
+app.cli.add_command(cli)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -32,6 +85,15 @@ def index():
     films = db_sess.query(Films).all()
 
     return render_template('index.html', films=films)
+
+
+@app.route('/film/<int:film_id>')
+def film_detail(film_id):
+    db_sess = db_session.create_session()
+    film = db_sess.query(Films).get(film_id)
+    if not film:
+        return abort(404)
+    return render_template("film_detail.html", film=film)
 
 
 @app.route('/registration', methods=['GET', 'POST'])
@@ -103,6 +165,13 @@ def create_admin():
     print(f"Администратор {username} успешно создан!")
 
 
+@app.route('/my_films')
+def my_films(): 
+    db_sess = db_session.create_session()
+    user_films = db_sess.query(Films).filter(Films.user_id == current_user.id).all()
+    return render_template("my_films.html", films=user_films)
+
+
 @app.route('/logout')
 @login_required
 def logout():
@@ -137,7 +206,10 @@ def add_film():
             title=new_film.title.data,
             description=new_film.description.data,
             director=new_film.director.data,
+            screenwriter=new_film.screenwriter.data,
+            duration=new_film.duration.data,
             average_rating=new_film.rating.data,
+            premiere=new_film.premiere.data,
             poster_path=poster_db_path,
             user_id=current_user.id
         )
@@ -145,7 +217,7 @@ def add_film():
         db_sess.commit()
         return redirect('/')
     return render_template('add_film.html',
-                           title='Добавление работы',
+                           title='Добавление фильма',
                            form=new_film)
 
 
--- forms/add_film.py
+++ forms/add_film.py
@@ -8,10 +8,13 @@ class FilmForm(FlaskForm):
     title = StringField('Название фильма', validators=[DataRequired()])
     description = TextAreaField('Описание фильма', validators=[DataRequired()])
     director = StringField('Режисер', validators=[DataRequired()])
+    screenwriter = StringField('Сценарист', validators=[DataRequired()])
+    duration = StringField('Длительность', validators=[DataRequired()])
     rating = SelectField('Ваша оценка',
                          choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'),
                                   (6, '6'), (7, '7'), (8, '8'), (9, '9'), (10, '10')],
                          coerce=int,
                          validators=[DataRequired()])
+    premiere = StringField('Премьера', validators=[DataRequired()])
     poster = FileField('Прекрепите постер', validators=[FileRequired()])
     submit = SubmitField('Применить')
